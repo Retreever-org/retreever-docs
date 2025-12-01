ac28f648e073312c3f0e5cc0c98b7c2929ac9641
rajumb0232 <rajugowda0232@gmail.com>
2025-12-01 18:27:32 +0530

    add basename for the router

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/retreever-docs/">
       <App />
     </BrowserRouter>
   </StrictMode>
